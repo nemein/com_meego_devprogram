7c86ead64390cdbe2eb9d6b89103071a031b2212
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-03-24 10:53:16 +0200

    Skeleton of DevProgram with a simple index page

---
+++ controllers/index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package com_meego_devprogram
+ * @author Ferenc Szekely, http://www.nemein.com
+ * @copyright The Midgard Project, http://www.midgard-project.org
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+class com_meego_devprogram_controllers_index
+{
+    var $mvc = null;
+    var $request = null;
+
+    /**
+     * Contructor; set mvc and i18n domain
+     *
+     * @param object request is a midgardmvc_core_request object
+     */
+    public function __construct(midgardmvc_core_request $request)
+    {
+        $this->request = $request;
+        $this->mvc = midgardmvc_core::get_instance();
+        $this->mvc->i18n->set_translation_domain('com_meego_devprogram');
+
+        $default_language = $this->mvc->configuration->default_language;
+
+        if (! isset($default_language))
+        {
+            $default_language = 'en_US';
+        }
+
+        $this->mvc->i18n->set_language($default_language, false);
+    }
+
+
+    /**
+     * Prepares and shows the index page (cmd-index)
+     *
+     * @param array args (not used)
+     */
+    public function get_index(array $args)
+    {
+    }
+}
+
+?>
