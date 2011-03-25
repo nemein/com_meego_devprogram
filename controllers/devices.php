8c620bd3bda784fdd959f80e97c6453c9f66246c
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-03-26 00:54:36 +0200

    simplified form creation; use common url generator; proper read, update, delete

--- controllers/devices.php
+++ controllers/devices.php
@@ -31,7 +31,7 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
 
         if (is_object($this->object))
         {
-            midgardmvc_core::get_instance()->head->set_title($this->object->title);
+            midgardmvc_core::get_instance()->head->set_title($this->object->name);
         }
     }
 
@@ -48,12 +48,10 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
      */
     public function get_url_read()
     {
-        return midgardmvc_core::get_instance()->dispatcher->generate_url
-        (
+        return com_meego_devprogram_utils::get_url(
             'device_read',
-            array('device_name' => $this->object->name),
-            $this->request
-        );
+            array('device_name' => $this->object->name
+        ));
     }
 
     /**
@@ -61,12 +59,10 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
      */
     public function get_url_update()
     {
-        return midgardmvc_core::get_instance()->dispatcher->generate_url
-        (
+        return com_meego_devprogram_utils::get_url(
             'device_update',
-            array('device_name' => $this->object->name),
-            $this->request
-        );
+            array('device_name' => $this->object->name
+        ));
     }
 
     /**
@@ -74,42 +70,8 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
      */
     public function load_form()
     {
-        $this->form = midgardmvc_helper_forms::create('com_meego_devprogram_device');
-
-        $name = $this->form->add_field('name', 'text', true);
-        $name->set_value($this->object->name);
-        $widget = $name->set_widget('text');
-        $widget->set_label($this->mvc->i18n->get('label_name'));
-
-        // @todo: add name tip
-
-        $model = $this->form->add_field('model', 'text', true);
-        $model->set_value($this->object->model);
-        $widget = $model->set_widget('text');
-        $widget->set_label($this->mvc->i18n->get('label_model'));
-
-        // @todo: add model tip
-
-        $type = $this->form->add_field('type', 'text', true);
-        $type->set_value($this->object->type);
-        $widget = $type->set_widget('text');
-        $widget->set_label($this->mvc->i18n->get('label_type'));
-
-        // @todo: add type tip
-
-        $description = $this->form->add_field('description', 'text', true);
-        $description->set_value($this->object->description);
-        $widget = $description->set_widget('html');
-        $widget->set_label($this->mvc->i18n->get('label_description'));
-
-        // @todo: add description tip
-
-        $url = $this->form->add_field('url', 'url', true);
-        $url->set_value($this->object->url);
-        $widget = $url->set_widget('text');
-        $widget->set_label($this->mvc->i18n->get('label_url'));
-
-        // @todo: add url tip
+        $this->form = midgardmvc_helper_forms_mgdschema::create($this->object, false, 'label_device_', 'tip_device_');
+        $this->form->set_submit('form-submit', $this->mvc->i18n->get('command_save'));
     }
 
     /**
@@ -124,6 +86,7 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
         {
             return;
         }
+        $this->data['my_devices'] = com_meego_devprogram_devutils::get_devices_of_current_user();
     }
 
     /**
@@ -136,8 +99,52 @@ class com_meego_devprogram_controllers_devices extends midgardmvc_core_controlle
      */
     public function get_read(array $args)
     {
-        // set owner flag
-        // @todo
+        $this->load_object($args);
+
+        $this->load_form();
+
+        if (! com_meego_devprogram_utils::is_current_user_creator_or_admin($this->object->guid))
+        {
+            $this->form->set_readonly(true);
+        }
+
+        $this->data['form'] =& $this->form;
+    }
+
+    /**
+     * Creates a new device
+     * But it makes sure that the name is unique
+     */
+    public function post_create(array $args)
+    {
+        parent::post_delete($args);
+    }
+
+    /**
+     * Prepares the delete page
+     */
+    public function get_delete(array $args)
+    {
+        parent::get_delete($args);
+
+        $this->data['index_url'] = com_meego_devprogram_utils::get_url('index', array());
+
+        $this->data['can_not_delete'] = com_meego_devprogram_progutils::any_open_program_uses_device($this->object->id);
+
+        $this->data['delete_question'] = $this->mvc->i18n->get('question_delete', null, array('device_name' => $this->object->name));
+    }
+
+    /**
+     * Deletes the device
+     * First it makes sure that all expired programs are deleted,
+     * otherwise a device can not be deleted if it is assigned to a program
+     */
+    public function post_delete(array $args)
+    {
+        # make sure all closed (expired) programs are marked deleted
+        # before attempting to delete a device
+        com_meego_devprogram_progutils::delete_expired_programs();
+        parent::post_delete($args);
     }
 }
 ?>
