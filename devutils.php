b4013ffd177e85422a663bc930188eb4d326d474
Ferenc Szekely <ferenc.szekely@gmail.com>
2011-03-26 00:55:56 +0200

    useful functions

--- devutils.php
+++ devutils.php
@@ -40,7 +40,7 @@ class com_meego_devprogram_devutils extends com_meego_devprogram_utils
         $qc = new midgard_query_constraint(
             new midgard_query_property('metadata.creator'),
             '=',
-            new midgard_query_value($user->guid)
+            new midgard_query_value($user->person)
         );
 
         $q->set_constraint($qc);
@@ -52,5 +52,126 @@ class com_meego_devprogram_devutils extends com_meego_devprogram_utils
         }
 
         return $retval;
+    }
+
+    /**
+     * Retrieves a device by its name
+     *
+     * @param string name
+     * @return object com_meego_debprogram device object
+     */
+    public function get_device_by_name($name = '')
+    {
+        $device = null;
+
+        if (strlen($name))
+        {
+            $storage = new midgard_query_storage('com_meego_devprogram_device');
+
+            $q = new midgard_query_select($storage);
+
+            $qc = new midgard_query_constraint(
+                new midgard_query_property('name'),
+                '=',
+                new midgard_query_value($name)
+            );
+
+            $q->set_constraint($qc);
+            $q->execute();
+
+            $devices = $q->list_objects();
+
+            if (count($devices))
+            {
+                $device = new com_meego_devprogram_device($devices[0]->guid);
+            }
+        }
+
+        return $device;
+    }
+
+    /**
+     * Retrieves devices created by user having the guid
+     *
+     * @param guid guid of the user
+     * @return array an array of com_meego_devprogram_device objects
+     */
+    private static function get_devices_by_creator_guid($guid = '')
+    {
+        $devices = array();
+
+        if (mgd_is_guid($guid))
+        {
+            $storage = new midgard_query_storage('com_meego_devprogram_device');
+
+            $q = new midgard_query_select($storage);
+
+            $qc = new midgard_query_constraint(
+                new midgard_query_property('metadata.creator'),
+                '=',
+                new midgard_query_value($guid)
+            );
+
+            $q->set_constraint($qc);
+            $q->execute();
+
+            // add read, delete, update urls to objects
+            $objects = $q->list_objects();
+
+            if (count($objects))
+            {
+                $mvc = midgardmvc_core::get_instance();
+
+                foreach ($objects as $object)
+                {
+                    $object->read_url = com_meego_devprogram_utils::get_url('device_read', array ('device_name' => $object->name));
+                    $object->update_url = com_meego_devprogram_utils::get_url('device_update', array ('device_name' => $object->name));
+                    $object->delete_url = com_meego_devprogram_utils::get_url('device_delete', array ('device_name' => $object->name));
+
+                    $devices[] = $object;
+                }
+            }
+        }
+
+        return $devices;
+    }
+
+    /**
+     * Retrieves devices created by the given user
+     *
+     * @param string login name (ie. user name) of the user
+     * @return array an array of com_meego_devprogram_device objects
+     */
+    public static function get_devices_of_user($login = '')
+    {
+        $devices = array();
+
+        if (! strlen($login))
+        {
+            return $devices;
+        }
+
+        // retrieve the user's guid based on the login name
+        $user_guid = self::get_guid_of_user($login);
+
+        return self::get_devices_by_creator_guid($user_guid);
+    }
+
+    /**
+     * Retrieves devices of the currently loged in user
+     *
+     * @return array an array of com_meego_devprogram_device objects
+     *         null if user is not logged in
+     */
+    public static function get_devices_of_current_user()
+    {
+        $user = self::require_login();
+
+        if (! is_object($user))
+        {
+            return null;
+        }
+
+        return self::get_devices_by_creator_guid($user->person);
     }
 }
